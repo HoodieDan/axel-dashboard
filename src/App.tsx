9258c51cbcf81646c378ffc6a9b79687ad3bf7b1
hoodiedan <obodedaniel3@gmail.com>
2025-02-22 02:17:22 +0100

    chore: Add Tailwind CSS integration and update styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import './styles/index.scss';
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='p-4'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,6 +18,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="font-bold underline p-8">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
